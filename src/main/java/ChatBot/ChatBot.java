c23ee2fb714cc2da5aabeb20015087db052406a9
Yaroslav <y.s.syrovatka@student.khai.edu>
2022-10-05 18:12:33 +0300

    ChatBot 2-st

--- src/main/java/ChatBot/ChatBot.java
+++ src/main/java/ChatBot/ChatBot.java
@@ -1,8 +1,18 @@
 package ChatBot;
+import java.util.Scanner;
 
 public class ChatBot {
     public void botName() {
         System.out.println("Hello! My name is Gustavo");
         System.out.println("I was created in 2022");
+        System.out.println("Please, remind me your name.");
+        String name = scannerString();
+        System.out.println("What a great name you have, " + name + "!");
+    }
+
+    public String scannerString() {
+        try (Scanner scanner = new Scanner(System.in)){
+            return scanner.nextLine();
+        }
     }
 }
