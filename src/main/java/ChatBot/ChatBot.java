67f68e7c195c39b87101706ff84509da99c32f62
Yaroslav <y.s.syrovatka@student.khai.edu>
2022-10-05 14:35:54 +0300

    ChatBot 1-st

---
+++ src/main/java/ChatBot/ChatBot.java
@@ -0,0 +1,8 @@
+package ChatBot;
+
+public class ChatBot {
+    public void botName() {
+        System.out.println("Hello! My name is Gustavo");
+        System.out.println("I was created in 2022");
+    }
+}
