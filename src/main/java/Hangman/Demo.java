6aac08fbda0bdafa2be84a7aa8a51efb3baa6871
Yaroslav <y.s.syrovatka@student.khai.edu>
2022-10-18 14:17:11 +0300

    hangman v.1

---
+++ src/main/java/Hangman/Demo.java
@@ -0,0 +1,8 @@
+package Hangman;
+
+public class Demo {
+    public static void main(String[] args) {
+        Hangmen hangmen = new Hangmen();
+        hangmen.anons();
+    }
+}
---
+++ src/main/java/Hangman/Hangmen.java
@@ -0,0 +1,8 @@
+package Hangman;
+
+public class Hangmen {
+    public void anons() {
+        System.out.println("HANGMAN");
+        System.out.println("The game will be available soon.");
+    }
+}
