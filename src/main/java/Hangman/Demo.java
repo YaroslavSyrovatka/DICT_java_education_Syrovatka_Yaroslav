3e03247e266850761e030d12d8a9150b25d06dc4
Yaroslav <y.s.syrovatka@student.khai.edu>
2022-10-23 17:14:58 +0300

    hangman last version :c

--- src/main/java/Hangman/Demo.java
+++ src/main/java/Hangman/Demo.java
@@ -1,9 +1,12 @@
 package Hangman;
 
+import java.util.Scanner;
+
 public class Demo {
     public static void main(String[] args) {
+        Scanner user = new Scanner(System.in);
         Hangmen hangmen = new Hangmen();
-        hangmen.anons();
-        hangmen.guessWord();
+        Menu menu = new Menu();
+        menu.hangmanMenu(user, hangmen);
     }
 }
--- src/main/java/Hangman/Hangmen.java
+++ src/main/java/Hangman/Hangmen.java
@@ -1,49 +1,117 @@
 package Hangman;
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class Hangmen {
-    private String[] words = new String[]{"python", "java", "javascript", "kotlin"};
+    private List<String> words = Arrays.asList("python", "java", "javascript", "kotlin");
 
-    private final int randomIndex = new Random().nextInt(words.length);
+    private Scanner userAnswer = new Scanner(System.in);
+
+    private Random rand = new Random();
+
+    private String word = words.get(rand.nextInt(words.size()));
+
+    private List<Character> userGuess = new ArrayList<>();
+
+    private int wrongAnswer = 0;
+
+    public void game() {
+        while(true) {
+            if (printResult()) {
+                System.out.println("You guessed the word java!\n" +
+                        "You survived!");
+                userAnswer.close();
+                break;
+            }
+
+            if (!getUserGuessGhar()) {
+                wrongAnswer++;
+                System.out.println("That letter doesn`t appear in the word");
+            }
+
+            if (wrongAnswer == 9) {
+                System.out.println("You lost!");
+                userAnswer.close();
+                break;
+            }
 
 
-    public void anons() {
-        System.out.println("HANGMAN");
-    }
-
-    public String hint(String word) {
-        String[] result = word.split("");
-        for (int i = 2; i < result.length; i++) {
-            result[i] = "-";
         }
-        return String.join("", result);
     }
 
+    public boolean printResult() {
+        int correctCount = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (userGuess.contains(word.charAt(i))) {
+                System.out.print(word.charAt(i));
+                correctCount++;
+            } else {
+                System.out.print("-");
+            }
+        }
+        System.out.println();
+        return word.length() == correctCount;
+    }
 
-    public void guessWord() {
-        Scanner in = new Scanner(System.in);
-        String randomWords = words[randomIndex];
-        System.out.print("Guess the word " + hint(randomWords) + ":> ");
-        String userWord = in.next();
-        in.close();
-        if (userWord.equals(randomWords)) {
-            System.out.println("You survived!");
+    public boolean getUserGuessGhar() {
+        System.out.print("Input a letter:>");
+        String latterGuess = userAnswer.nextLine();
+        String regExp = "[a-z]";
+        boolean correctWord = Pattern.matches(regExp,latterGuess);
+        boolean checkCorrect = true;
+        if (latterGuess.length() != 1) {
+            System.out.println("You should input a single letter.");
+        } else if (!correctWord){
+            System.out.println("Please enter a lowercase English letter.");
+        } else if (!word.contains(latterGuess)){
+            checkCorrect = false;
+        } else if (!userGuess.contains(latterGuess.charAt(0))){
+            userGuess.add((latterGuess.charAt(0)));
         } else {
-            System.out.println("You lost!");
+            System.out.println("You`ve already guessed this letter.");
         }
+
+        return checkCorrect;
     }
 
-    public String[] getWords() {
+    public List<String> getWords() {
         return words;
     }
 
-    public void setWords(String[] words) {
+    public void setWords(List<String> words) {
         this.words = words;
     }
 
+    public Scanner getUserAnswer() {
+        return userAnswer;
+    }
 
+    public void setUserAnswer(Scanner userAnswer) {
+        this.userAnswer = userAnswer;
+    }
+
+    public Random getRand() {
+        return rand;
+    }
+
+    public void setRand(Random rand) {
+        this.rand = rand;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public List<Character> getUserGuess() {
+        return userGuess;
+    }
+
+    public void setUserGuess(List<Character> userGuess) {
+        this.userGuess = userGuess;
+    }
 }
