c4e6d32bcb85e67acf0e32c2d4a17c1706dbce66
Yaroslav <y.s.syrovatka@student.khai.edu>
2022-11-01 14:46:53 +0300

    Coffee Machine st-1

---
+++ src/main/java/CoffeeMachine/CoffeeMachineDemo.java
@@ -0,0 +1,8 @@
+package CoffeeMachine;
+
+public class CoffeeMachineDemo {
+    public static void main(String[] args) {
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+        coffeeMachine.printText();
+    }
+}
