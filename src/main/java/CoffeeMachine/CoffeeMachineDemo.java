8dd77ace2137bc838ee70a633e5722cb9bb06003
Yaroslav <y.s.syrovatka@student.khai.edu>
2022-11-02 16:52:17 +0300

    Coffee Machine st-6

--- src/main/java/CoffeeMachine/CoffeeMachineDemo.java
+++ src/main/java/CoffeeMachine/CoffeeMachineDemo.java
@@ -5,6 +5,7 @@ import java.util.concurrent.atomic.AtomicLong;
 public class CoffeeMachineDemo {
     public static void main(String[] args) {
         CoffeeMachine coffeeMachine = new CoffeeMachine();
-        coffeeMachine.menu();
+        UserInputClass userInputClass = new UserInputClass();
+        coffeeMachine.menu(userInputClass);
     }
 }
